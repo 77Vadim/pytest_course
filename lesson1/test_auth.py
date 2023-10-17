c1c8278f8ced6fbd0acf3a6fd96e370f42e1f342
Vadim <143993763+77Vadim@users.noreply.github.com>
2023-10-17 16:33:54 +0600

    fixture

--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -2,13 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time 
 
-driver = webdriver.Chrome()
 
-
-
-def test_login_form():
-    driver.get("https://www.saucedemo.com/")
-
+def test_login_form(driver):
     username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
     username_field.send_keys("standard_user")
 
@@ -18,10 +13,8 @@ def test_login_form():
     login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
     login_button.click()
 
-    time.sleep(5)
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
 
-    driver.quit()
 
 
 
